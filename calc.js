59a2349f61a8fea6482120172f1a1e66672ea151
RyanKirkland07 <132314358+RyanKirkland07@users.noreply.github.com>
2026-05-28 19:21:38 +1000

    added operate function to determine the operation and call the corresponding function

--- calc.js
+++ calc.js
@@ -13,3 +13,35 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+let num1;
+let num2;
+
+let operator;
+
+function operate(leftNum, rightNum, operatorChar){
+    let out;
+    switch(operatorChar){
+        case "+":
+            out = add(leftNum, rightNum);
+            break;
+        case "-":
+            out = subtract(leftNum, rightNum);
+            break;
+        case "*":
+            out = multiply(leftNum, rightNum);
+            break;
+        case "/":
+            out = divide(leftNum, rightNum);
+            break;
+        default:
+            console.log("Invaid operator");
+            break;
+    }
+    console.log(out);
+}
+
+operate(3, 2, "+");
+operate(8, 5, "-");
+operate(2, 4, "*");
+operate(9, 3, "/");
